2d01dbc21543976395fa48fa5c3a7b9a7b32a15f
Nanki Sekhon <nankisekhon01@gmail.com>
2025-04-14 22:39:12 -0700

    updated pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
 import React  from 'react';
 import NavBar from './components/navbar';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import Writing from './pages/Writing';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
-  const title = "Nanki's Beautiful Website";
-  //const pinterestLink = "https://www.pinterest.com/nankisekhon/";
+
   return (
       
       <>
-      <NavBar></NavBar>
-      <h1>YO from {title}!</h1>
+      <NavBar/>
+      <div className="navRouting">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/writing" element={<Writing />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contactme" element={<Contact />} />
+        </Routes>
+
+      </div>
       </>
 
       
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,143 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
 
 // provides navigation
 function Navbar() {
-  //onst [click, setClick] = useState(false);
-  //const [button, setButton] = useState(true);
-
-  //const handleClick = () => setClick(!click);
-  //const closeMobileMenu = () => setClick(false);
-/*
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-  
-
-  window.addEventListener('resize', showButton);
-*/
 
 return (
   <nav className='nav'>
-    <a href="/">nks</a>
+    <Link to="/" className="site-title">nks</Link>
     <ul>
-      <li>
-        <a href="/projects">Projects</a>
-      </li>
-      <li>
-        <a href="/writing">Writing</a>
-      </li>
-      <li>
-        <a href="/contactme">Say Hi!</a>
-      </li>
+      <CustomLink to="/projects">Projects</CustomLink>
+      <CustomLink to="/writing">Writing</CustomLink>
+      <CustomLink to="/resume">Resume</CustomLink>
+      <CustomLink to="/contactme">Say Hi!</CustomLink>
     </ul>
   </nav>
 )
 }
 
-/*
+function CustomLink({to, children, ...props}) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({path: resolvedPath.pathname, end: true});
   return (
-    <>
-      <nav className='navbar'>
-        <a href="/" className="title">nks</a>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-nks-logo' onClick={closeMobileMenu}>
-            nks
-            <i class='fab fa-typo3' />
-          </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className='nav-menu-list'>
-
-            <li className='nav-item'>
-              <NavLink 
-                to='/' 
-                className={({isActive}) => 
-                  isActive ? 'nav-links active' : 'nav-links'
-                }
-                onClick={closeMobileMenu}
-                title= 'See Where All the Magic Lives'
-                aria-label="Home Page"
-                tabIndex={0}
-                >
-                Home
-              </NavLink>
-            </li>
-
-            <li className='nav-item'>
-              <NavLink
-                to='/resume'
-                className={({isActive}) => 
-                  isActive ? 'nav-links active' : 'nav-links'
-                }
-                onClick={closeMobileMenu}
-                title= 'See Where the Magic was Built'
-                aria-label="Nanki's Resume"
-                tabIndex={1}
-              >
-                Resume
-              </NavLink>
-            </li>
-
-            <li className='nav-item'>
-              <NavLink
-                to='/projects'
-                className={({isActive}) => 
-                  isActive ? 'nav-links active' : 'nav-links'
-                }
-                onClick={closeMobileMenu}
-                title= 'See the Magic'
-                aria-label="Nanki's Projects"
-                tabIndex={2}
-              >
-                Projects
-                <i className='fas fa-folder'></i>
-              </NavLink>
-
-            </li>
-
-            <li className='nav-item'>
-              <NavLink
-                to='/writing'
-                className={({isActive}) => 
-                  isActive ? 'nav-links active' : 'nav-links'
-                }
-                onClick={closeMobileMenu}
-                title= 'See Where the Magic Takes Ahold of a Pen'
-                aria-label="Nanki's Writing"
-                tabIndex={3}
-              >
-                Writing
-              </NavLink>
-            </li>
-
-            <li className='nav-item'>
-              <NavLink
-                to='/contactme'
-                className={({isActive}) => 
-                  isActive ? 'nav-links active' : 'nav-links'
-                }
-                onClick={closeMobileMenu}
-                title= 'Contact the Magic'
-                aria-label="Contact Nanki"
-                tabIndex={4}
-              >
-                Say Hi!
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
+  <li className={isActive ? 'active' : ''}>
+        <Link to={to}{...props}>{children}</Link>
+  </li>
+  )
 }
-*/
 
 export default Navbar;
